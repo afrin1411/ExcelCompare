a1463cc177f79a936d5b3eabcdf88a7cefa4d7b8
Afrin Anjum <afrinanjum316@gmail.com>
2022-07-14 12:31:22 +0530

    added projct

---
+++ ExcelCompare/frmHome.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ExcelCompare
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtCompareColumns = new System.Windows.Forms.TextBox();
+            this.txtExcludedColumn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(138, 36);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(169, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ExcelFile Path: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Columns To Compare :";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(313, 27);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(59, 29);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtCompareColumns
+            // 
+            this.txtCompareColumns.Location = new System.Drawing.Point(138, 62);
+            this.txtCompareColumns.Name = "txtCompareColumns";
+            this.txtCompareColumns.Size = new System.Drawing.Size(169, 20);
+            this.txtCompareColumns.TabIndex = 4;
+            // 
+            // txtExcludedColumn
+            // 
+            this.txtExcludedColumn.Location = new System.Drawing.Point(138, 88);
+            this.txtExcludedColumn.Name = "txtExcludedColumn";
+            this.txtExcludedColumn.Size = new System.Drawing.Size(169, 20);
+            this.txtExcludedColumn.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Columns To Exclude :";
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(138, 146);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 7;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 205);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.txtExcludedColumn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCompareColumns);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Name = "frmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtCompareColumns;
+        private System.Windows.Forms.TextBox txtExcludedColumn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCompare;
+    }
+}
---
+++ ExcelCompare/frmHome.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelCompare
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "d:\\";
+                openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet1;
+            Excel.Range rangeSheet1;
+
+            string str;
+            int rCnt;
+            int cCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(txtPath.Text, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet1 = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            var xlWorkSheet2 = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);
+            //var xlWorkSheet3 = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(3);
+            var diffList = new List<string>();
+
+            try
+            {
+
+                rangeSheet1 = xlWorkSheet1.UsedRange;
+                rw = rangeSheet1.Rows.Count;
+                cl = rangeSheet1.Columns.Count;
+
+                var rowdataSheet1 = new Dictionary<string, int>();
+                var columnsToExclude = txtExcludedColumn.Text.Split(',');
+               
+
+                for (rCnt = 1; rCnt <= rw; rCnt++)
+                {
+                    str = "";
+                   
+                    for (cCnt = 1; cCnt <= cl; cCnt++)
+                    {
+                        var rangeSelected = (rangeSheet1.Cells[rCnt, cCnt] as Excel.Range);
+                        var address = rangeSelected.Address;
+                        if (!columnsToExclude.Contains(address.Substring(1, 1)))
+                        {
+                            str += (string)rangeSelected.Value2 + ",";
+                        }
+                    }
+                    str = str.TrimEnd(',');
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        //if (!rowdataSheet1.ContainsKey(str)) //TO CHECK DUPLICATE
+                        {
+                            rowdataSheet1.Add(str.ToLower(), rCnt);
+                        }
+                                              
+                    }
+                }
+
+                //sheet2
+                var rangeSheet2 = xlWorkSheet2.UsedRange;
+                rw = rangeSheet2.Rows.Count;
+                cl = rangeSheet2.Columns.Count;
+
+                int k = 1;
+                for (rCnt = 1; rCnt <= rw; rCnt++)
+                {
+                    str = "";
+
+                    for (cCnt = 1; cCnt <= cl; cCnt++) 
+                    {
+                        var rangeSelected = (rangeSheet2.Cells[rCnt, cCnt] as Excel.Range);
+                        var address = rangeSelected.Address;
+                        if (!columnsToExclude.Contains(address.Substring(1, 1)))
+                        {
+                            str += (string)rangeSelected.Value2 + ",";
+                        }
+                    }
+                    str = str.TrimEnd(',');
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        if (!rowdataSheet1.ContainsKey(str.ToLower()))
+                        {
+                            diffList.Add(str);
+                        }
+                    }
+                }
+                File.WriteAllLines("d:\\diff_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".csv", diffList.ToArray());
+                MessageBox.Show("Done");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+
+            }
+            finally
+            {
+                xlWorkBook.Close(true, null, null);
+                xlApp.Quit();
+
+                Marshal.ReleaseComObject(xlWorkSheet1);
+                Marshal.ReleaseComObject(xlWorkBook);
+                Marshal.ReleaseComObject(xlApp);
+
+            }
+
+        }
+
+        
+    }
+}
